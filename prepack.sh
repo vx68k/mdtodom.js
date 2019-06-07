f849e41e637795871176b8daf31d0ddcb8d85c67
Kaz Nishimura <kazssym@linuxfront.com>
2019-06-07 09:50:45 +0900

    Set the default value for the package version

--- prepack.sh
+++ prepack.sh
@@ -2,10 +2,12 @@
 
 distdir="`pwd`"/dist
 
+PACKAGE_VERSION="${npm_package_version:-(unversioned)}"
+
 rm -rf "$distdir" || exit 1
 mkdir -p "$distdir" || exit 1
 
 for file in *.js extras/*.js; do
-    sed -e "s/[@]PACKAGE_VERSION[@]/$npm_package_version/g" \
+    sed -e "s/[@]PACKAGE_VERSION[@]/$PACKAGE_VERSION/g" \
         $file > "$distdir"/${file#*/} || exit 1; \
 done
