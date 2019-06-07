a831e8c3a6256cbafcfb71122c84c3418ec86363
Kaz Nishimura <kazssym@linuxfront.com>
2019-06-07 10:02:13 +0900

    Also embed the package name

--- prepack.sh
+++ prepack.sh
@@ -2,12 +2,15 @@
 
 distdir="`pwd`"/dist
 
+PACKAGE_NAME="${npm_package_name:-mdtodom}"
 PACKAGE_VERSION="${npm_package_version:-(unversioned)}"
 
 rm -rf "$distdir" || exit 1
 mkdir -p "$distdir" || exit 1
 
 for file in *.js extras/*.js; do
-    sed -e "s/[@]PACKAGE_VERSION[@]/$PACKAGE_VERSION/g" \
+    sed \
+        -e "s,[@]PACKAGE_NAME[@],$PACKAGE_NAME,g" \
+        -e "s,[@]PACKAGE_VERSION[@],$PACKAGE_VERSION,g" \
         $file > "$distdir"/${file#*/} || exit 1; \
 done
