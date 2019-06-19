efaca569ab18002d4ed1edf16453d30147c0a211
Kaz Nishimura <kazssym@linuxfront.com>
2019-06-19 17:12:48 +0900

    Use terser to generate minified scripts

--- prepack.sh
+++ prepack.sh
@@ -5,12 +5,18 @@ distdir="`pwd`"/dist
 PACKAGE_NAME="${npm_package_name:-mdtodom}"
 PACKAGE_VERSION="${npm_package_version:-(unversioned)}"
 
+SCRIPTS="mdtodom.js mdview.js extras/*.js"
+
 rm -rf "$distdir" || exit 1
 mkdir -p "$distdir" || exit 1
 
-for file in *.js extras/*.js; do
+for file in $SCRIPTS; do
+    distfile=${file##*/}
     sed \
         -e "s,[@]PACKAGE_NAME[@],$PACKAGE_NAME,g" \
         -e "s,[@]PACKAGE_VERSION[@],$PACKAGE_VERSION,g" \
-        $file > "$distdir"/${file#*/} || exit 1; \
+        $file > "$distdir"/$distfile || exit 1
+    node ./node_modules/terser/bin/uglifyjs \
+        "$distdir"/$distfile \
+        --module --output "$distdir"/${distfile%.js}.min.js || exit 1
 done
